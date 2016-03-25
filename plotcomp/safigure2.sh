78aa08bc7c61f6e603df9f28a6484c998adfc9e9
Luke Shingles <luke.shingles@gmail.com>
2016-03-25 22:54:47 +0000

    remove unneeded scripts

--- plotcomp/safigure2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-../getmodelabundances.py ../m3z01-ths8-pmz1/comp/ns125_0006940.cmp p c13 n14 > m3z01-ths8pmz1-0006940.csv
-./plotpmz.py m3z01-ths8pmz1-0006940.csv
-rm m3z01-ths8pmz1-0006940.csv
-../getmodelabundances.py ../m3z01-ths8-pmz1/comp/ns125_0007184.cmp p c13 n14 > m3z01-ths8pmz1-0007184.csv
-./plotpmz.py m3z01-ths8pmz1-0007184.csv
-rm m3z01-ths8pmz1-0007184.csv
