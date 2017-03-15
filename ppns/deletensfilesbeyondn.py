a39824e57c533df1a4b396a952da34e0ab6c7ece
Luke Shingles <luke.shingles@gmail.com>
2017-03-15 16:25:12 +0000

    Add python3 compatibility

--- ppns/deletensfilesbeyondn.py
+++ ppns/deletensfilesbeyondn.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 import os
-#from os import walk
+# from os import walk
 
-print r'This program will show and optionally delete ns????xxxxxxx.??? files where xxxxxxx is greater or equal to nmod_min'
+print(r'This program will show and optionally delete ns????xxxxxxx.??? files where xxxxxxx is greater or equal to nmod_min')
 
-nmodmin = int(raw_input("nmod_min:"))
+# Fix Python 2.x.
+try:
+    input = raw_input
+except NameError:
+    pass
+
+nmodmin = int(input("nmod_min:"))
 
 compfilelist = []
 for (dirpath, dirnames, filenames) in os.walk("./"):
-    compfilelist.extend([fn for fn in filenames if ((fn[:2]=='ns' and len(fn)==17) and int(fn[-11:-4]) >= nmodmin)])
+    compfilelist.extend([fn for fn in filenames if ((fn[:2] == 'ns' and len(fn) == 17) and int(fn[-11:-4]) >= nmodmin)])
     break
 
-print '\n'.join(compfilelist)
+print('\n'.join(compfilelist))
 
-if raw_input("Delete these files? (y/n):")=='y':
+if input("Delete these files? (y/n):") == 'y':
     for filename in compfilelist:
-        print 'deleting ' + filename
+        print('deleting ' + filename)
         os.remove(filename)
